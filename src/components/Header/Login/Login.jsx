a3f6b679cc1bec2e00516853c6b52b1aacdd9b4b
Sergey Deev <serg1deev@gmail.com>
2018-11-29 19:52:31 +0200

    improved requests

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -13,37 +13,166 @@ class Login extends Component {
         const linkRequestToken = `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`;
         const linkCreateSessionWithLogin = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
         const linkCreateSession = `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`;
-        fetch(linkRequestToken)
-            .then(response => response.json())
+
+        const fetchApi = (url, options = {}) => {
+            return new Promise((resolve, reject) => {
+                fetch(url, options)
+                    .then(response => {
+                        if(response.status < 400){
+                            return response.json();
+                        } else{
+                            throw response;
+                        }
+                    })
+                    .then(data => {
+                        resolve(data);
+                        }).catch(response => {
+                        response.json().then(error => {
+                            reject(error);
+                        })
+                    })
+
+            })
+        };
+
+        fetchApi(linkRequestToken)
+        .then(data => {
+            console.log("data",data);
+            return fetchApi(linkCreateSessionWithLogin,{
+                        method: "POST",
+                        mode: "cors",
+                        headers: {
+                            "Content-type": "application/json"
+                        },
+                        body: JSON.stringify({
+                                username: "sergluka",
+                                password: "apitarakan1972",
+                                request_token: data.request_token
+                            })
+                    })
+            })
             .then(data => {
-                //console.log(data);
-                fetch(linkCreateSessionWithLogin, {
+                console.log("succes",data);
+                fetchApi(linkCreateSession, {
                     method: "POST",
                     mode: "cors",
                     headers: {
                         "Content-type": "application/json"
                     },
                     body: JSON.stringify({
-                        username: "sergluka",
-                        password: "apitarakan1972",
                         request_token: data.request_token
                     })
-                })
-                    .then(response => response.json()).then(data => {
-                        //console.log(data);
-                        fetch(linkCreateSession, {
-                            method: "POST",
-                            mode: "cors",
-                            headers: {
-                                "Content-type": "application/json"
-                            },
-                            body: JSON.stringify({
-                                request_token: data.request_token
-                            })
-                        })
-                        .then(response => response.json()).then(data => console.log("session", data))
-                    });
+                }
+                )
             })
+            .then(data => {
+                console.log("session", data);
+            })
+            .catch(error => console.log("error", error));
+
+
+
+
+
+
+        //---2-----
+        // const getRequestToken = () => {
+        //     return new Promise((resolve, reject) => {
+        //         fetch(linkRequestToken)
+        //             .then(response => {
+        //                 if(response.status < 400){
+        //                     return response.json();
+        //                 } else{
+        //                     throw response;
+        //                 }
+        //             })
+        //             .then(data => {
+        //                 resolve(data);
+        //                 }).catch(response => {
+        //                 response.json().then(error => {
+        //                     reject(error);
+        //                 })
+        //             })
+        //
+        //     })
+        // }
+        //
+        // const validateWithLogin = body => {
+        //     return new Promise((resolve, reject) => {
+        //         fetch(linkCreateSessionWithLogin,{
+        //                     method: "POST",
+        //                     mode: "cors",
+        //                     headers: {
+        //                         "Content-type": "application/json"
+        //                     },
+        //                     body: JSON.stringify(body)
+        //                 })
+        //             .then(response => {
+        //                 if(response.status < 400){
+        //                     return response.json();
+        //                 } else{
+        //                     throw response;
+        //                 }
+        //             })
+        //             .then(data => {
+        //                 resolve(data);
+        //                 })
+        //                 .catch(response => {
+        //                 response.json().then(error => {
+        //                     reject(error);
+        //                 })
+        //             })
+        //
+        //     })
+        // }
+        //
+        // getRequestToken()
+        // .then(data => {
+        //     console.log("data",data);
+        //     return validateWithLogin({
+        //                     username: "serglukas",
+        //                     password: "apitarakan1972",
+        //                     request_token: data.request_token
+        //                 })
+        //     })
+        //     .then(data => console.log("succes",data))
+        //     .catch(error => console.log("error", error));
+
+
+        //---1---
+        // fetch(linkRequestToken)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         //console.log(data);
+        //         //2
+        //         fetch(linkCreateSessionWithLogin, {
+        //             method: "POST",
+        //             mode: "cors",
+        //             headers: {
+        //                 "Content-type": "application/json"
+        //             },
+        //             body: JSON.stringify({
+        //                 username: "sergluka",
+        //                 password: "apitarakan1972",
+        //                 request_token: data.request_token
+        //             })
+        //         })
+        //             .then(response => response.json()).then(data => {
+        //                 //console.log(data);
+        //                 //3
+        //                 fetch(linkCreateSession, {
+        //                     method: "POST",
+        //                     mode: "cors",
+        //                     headers: {
+        //                         "Content-type": "application/json"
+        //                     },
+        //                     body: JSON.stringify({
+        //                         request_token: data.request_token
+        //                     })
+        //                 })
+        //                 .then(response => response.json()).then(data => console.log("session", data))
+        //             });
+        //     })
     };
     render(){
         return(
