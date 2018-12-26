6ea21bd53dc1d5b6e956a907d685a438b79ba9d7
Sergey Deev <serg1deev@gmail.com>
2018-12-26 16:39:41 +0200

    added POST request for list of favorite movie

--- src/components/Movies/Favorites.jsx
+++ src/components/Movies/Favorites.jsx
@@ -2,18 +2,32 @@ import React from 'react'
 import UIIcon from '../UIComponents/UIIcon'
 import AppContextHOC from "../HOC/AppContextHOC";
 import AddMovieToListHOC from "../HOC/AddMovieToListHOC"
-
+import CallApi from "../../api/api"
 
 class Favorites extends React.Component{
     constructor(props){
         super(props);
-        this.state= {
+        this.state = {
             isAdded: this.props.favoriteMovies.includes(this.props.item.id)
         }
     }
 
     addFavorite = () => {
-        const { user, session_id, item } = this.props;
+        const { user, item, sessionId } = this.props;
+        this.setState(prevState => ({
+            ...prevState,
+            isAdded: !prevState.isAdded
+        }));
+        CallApi.post(`/account/${user.id}/favorite`, {
+            params: {
+                sessionId: sessionId
+            },
+            body: {
+                "media_type": "movie",
+                "media_id": item.id,
+                "favorite": this.state.isAdded
+            }
+        })
     };
 
     render(){
