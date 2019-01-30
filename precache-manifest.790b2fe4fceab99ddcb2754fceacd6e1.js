11f9bb84390148b7af838e077eac87a4de8f4180
Khanevich <serg1deev@gmail.com>
2019-01-30 19:19:07 +0200

    Updates

---
+++ precache-manifest.790b2fe4fceab99ddcb2754fceacd6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2746d1ef798dcd6887b",
+    "url": "/watcher/static/js/runtime~main.a2746d1e.js"
+  },
+  {
+    "revision": "aa1627d5d140a5d90c77",
+    "url": "/watcher/static/js/main.aa1627d5.chunk.js"
+  },
+  {
+    "revision": "f04423d1735c3cf7f6a5",
+    "url": "/watcher/static/js/1.f04423d1.chunk.js"
+  },
+  {
+    "revision": "aa1627d5d140a5d90c77",
+    "url": "/watcher/static/css/main.a9c5784e.chunk.css"
+  },
+  {
+    "revision": "f04423d1735c3cf7f6a5",
+    "url": "/watcher/static/css/1.f8be65f6.chunk.css"
+  },
+  {
+    "revision": "8e953339a220990bf60ffac502d21c69",
+    "url": "/watcher/index.html"
+  }
+];
